10cfc76dd4088d44ac82196ab985d333fc2a1ecc
Will Hancock <will@willhancock.net>
2017-01-09 23:16:20 -0500

    adding images, fixed image page css

---
+++ util/builddb.py
@@ -0,0 +1,12 @@
+from os import listdir
+import json
+
+photo_path = "../photos"
+out_path = "../data/photos.json"
+
+photos = listdir(photo_path)
+
+photo_list_json = json.dumps(photos)
+
+with open(out_path, 'w') as photo_db:
+	photo_db.write(photo_list_json)
